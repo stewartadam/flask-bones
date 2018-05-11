babbf6fa56310b0172933d444e3c7e2a6f9e158e
Stewart Adam <s.adam@diffingo.com>
2018-05-11 13:39:43 -0700

    Add flask-failsafe wrapper to serve.py

--- serve.py
+++ serve.py
@@ -1,3 +1,8 @@
-from app import create_app, config
+from flask_failsafe import failsafe
 
-app = create_app(config.dev_config)
+@failsafe
+def create_app_safe():
+  from app import create_app, config
+  return create_app(config.dev_config)
+
+app = create_app_safe()
